2852b09290abb2cb78640a3fcfe3514dd19d2436
Ig0or <igorprovensi@gmail.com>
2021-12-29 15:49:54 -0300

    ajustado menu sidebar

--- sistema/services/paciente_service.py
+++ sistema/services/paciente_service.py
@@ -1,7 +1,13 @@
 from ..models import Paciente
 
+
 def cadastrar_paciente(paciente):
     Paciente.objects.create(nome=paciente.nome, data_nascimento=paciente.data_nascimento, email=paciente.email, telefone=paciente.telefone, cpf=paciente.cpf, profissao=paciente.profissao, endereco=paciente.endereco)
 
+
 def listar_pacientes():
     return Paciente.objects.all()
+
+
+def listar_paciente_id(id):
+    return Paciente.objects.get(id=id)
